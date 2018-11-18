9c5ab813f83dd4797c73276649cbeefab5d1320b
Mingsong Zhai <15mz43@queensu.ca>
2018-11-18 15:59:02 -0500

    add database feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtDebug>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //create database
+    QSqlDatabase database;
+    if (QSqlDatabase::contains("qt_sql_default_connection"))
+    {
+        database = QSqlDatabase::database("qt_sql_default_connection");
+    }
+    else
+    {
+        database = QSqlDatabase::addDatabase("QSQLITE");
+        database.setDatabaseName("Database.db");
+        database.setUserName("");
+        database.setPassword("123456");
+    }
+    //open database
+    if (!database.open())
+    {
+        qDebug()<<"Errror: Failed to connect to database."<< database.lastError();
+    }
+    else
+    {
+        qDebug()<<"Succeed to connect to database.";
+    }
     MainWindow w;
     w.show();
 
